8eb30689b31f74bc0d7a62db4c36c97caa15fe7c
Daniel Underwood <daniel.underwood13@gmail.com>
2016-02-28 22:32:23 -0500

    Allow wildcard filenames

--- analysis/plot.py
+++ analysis/plot.py
@@ -16,25 +16,29 @@ def plot_data(currents, voltages, filenames):
     # Gather data into pandas dataframe
     data_frame = pandas.DataFrame({'Current': currents, 'Voltage': voltages, 'File': filenames})
 
-    return seaborn.lmplot(x='Voltage', y='Current', col='File', data=data_frame, fit_reg=False)
+    return seaborn.lmplot(x='Voltage', y='Current', col='File', data=data_frame, fit_reg=False, col_wrap=4)
 
 if __name__ == '__main__':
 
     # Set up parset to get filename
     parser = ArgumentParser(description='Load and plot data file for diode characterization')
-    parser.add_argument('filename', metavar='FILENAME', type=str)
     parser.add_argument('resistance', metavar='RESISTANCE', type=float)
+    parser.add_argument('filenames', nargs='*')
 
-    filename = parser.parse_args().filename
+    filenames_arg = parser.parse_args().filenames
     resistance = parser.parse_args().resistance
 
     # Load data from file
-    data = get_data(filename)
+    currents = []
+    voltages = []
+    filenames = []
+    for filename in filenames_arg:
+        data = get_data(filename)
+        currents += [voltage / resistance for voltage in data['resistor1_voltage']]
+        voltages += [datum for datum in data['dut_voltage']]
 
-    # Build data
-    currents = [voltage / resistance for voltage in data['resistor1_voltage']]
-    voltages = data['dut_voltage']
-    filenames = data['filename']
+        # Add filename for each point in data
+        filenames += [filename for datum in data['index']]
 
     plot = plot_data(currents, voltages, filenames)
     seaborn.plt.show()
